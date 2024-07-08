f4248aecd3fefe3ddeba350d6f0290cfe8c73a3a
mr-scott32 <128337536+mr-scott32@users.noreply.github.com>
2024-07-08 19:25:57 +1000

    still working on game

--- questionable_tkinterface.py
+++ questionable_tkinterface.py
@@ -11,25 +11,56 @@ root.config(bg='peachpuff2')
 map = PhotoImage(file='qtgfx/map.png')
 map_lbl = Label(root, image=map, bg='white', relief=SUNKEN).grid(row=0, column=0, columnspan=3)
 
-def statueScreen():
+def closeOnlyScreen(img_path, msg, txt):
     window = Toplevel(root)
-    window.title('Statue')
-    img = PhotoImage(file='qtgfx/statue.png')
-    img_label = Label(window, image=img, bg='white', relief=SUNKEN)
+    window.title(txt)
+    window.config(bg='peachpuff2')
+    img = PhotoImage(file=img_path)
+    img_label = Label(window, image=img, bg='peachpuff2', relief=SUNKEN)
     img_label.grid(row=0, column=0, columnspan=3)
     img_label.image = img
 
-    T = Text(window, height=3, width=50)
+    T = Text(window, height=3, width=45, bg='bisque')
     T.grid(column=1, row=2)
-    t_lbl = Label(window, text='Statue of Solace').grid(column=1, row=1)
-
-    msg = 'If you open the chest, you will discover its \neffects.'
+    t_lbl = Label(window, text=txt, bg='bisque').grid(column=1, row=1, padx=5, pady=5)
     T.insert(END, msg)
 
+    close_btn = Button(window, text='Close', command=window.destroy).grid(column=1, row=4, padx=5, pady=5)
 
+def gameOver():
+    root.destroy()
 
-statue_btn = Button(root, text='Check Statue', command=statueScreen, bg='peachpuff4', width=15).grid(row=2, column=0, padx=5, pady=5, sticky=W) 
-chest_btn = Button(root, text='Check Chest', bg='peachpuff4', width=15).grid(row=2, column=1, padx=5, pady=5, sticky=W)
+def decisionScreen(img_path, msg, txt):
+    window = Toplevel(root)
+    window.title(txt)
+    window.config(bg='peachpuff2')
+    img = PhotoImage(file=img_path)
+    img_label = Label(window, image=img, bg='peachpuff2', relief=SUNKEN)
+    img_label.grid(row=0, column=0, columnspan=3)
+    img_label.image = img
+
+    T = Text(window, height=3, width=45, bg='bisque')
+    T.grid(column=1, row=2)
+    t_lbl = Label(window, text=txt, bg='bisque').grid(column=1, row=1, padx=5, pady=5)
+    T.insert(END, msg)
+
+    
+#We use 'lambda' if we want to pass parameters to our functions in a Button command.
+statue_btn = Button(root, text='Check Statue', 
+                    command=lambda: closeOnlyScreen('qtgfx/statue.png',
+                    'If you open the chest, you will discover its \neffects.',
+                    'Statue of Solace'), 
+                    bg='peachpuff4', 
+                    width=15).grid(row=2, column=0, padx=5, pady=5, sticky=W) 
+
+curtain_btn = Button(root, text='Check Curtain',
+                     command=lambda: closeOnlyScreen('qtgfx/curtain.png',
+                    'Open the sarcophagus to discover infinite \nemeralds.',
+                    'Curious Curtain'),
+                    bg='peachpuff4',
+                    width=15).grid(row=2, column=1, padx=5, pady=5, sticky=W)
+
+chest_btn = Button(root, text='Check Chest', bg='peachpuff4', width=15).grid(row=3, column=1, padx=5, pady=5, sticky=W)
 
 
 
